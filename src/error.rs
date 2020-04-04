5cd3868ff8864b449545ba4c436ee8c81c8a4a1b
Domenico Gabriele <domenico@blockstream.com>
2020-04-04 23:43:10 +0200

    turn errors into appropriate rpc responses

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use crate::util::RpcResponse;
 use bitcoin::blockdata::transaction::ParseOutPointError;
 use bitcoin::util::address;
 use serde_json::Value;
@@ -53,5 +54,19 @@ impl From<address::Error> for PluginError {
 impl From<reqwest::Error> for PluginError {
     fn from(e: reqwest::Error) -> Self {
         PluginError::Http(e)
+    }
+}
+
+impl PluginError {
+    pub fn get_rpc_error(&self, id: u64) -> RpcResponse {
+        match self {
+            PluginError::Message(s) => RpcResponse::error(id, s.to_string()),
+            PluginError::Json(_e) => RpcResponse::error(id, "json error".to_string()),
+            PluginError::BitcoinSecpError(_e) => RpcResponse::error(id, "secp error".to_string()),
+            PluginError::BitcoinKeyError(_e) => RpcResponse::error(id, "bad private key".to_string()),
+            PluginError::BitcoinOutpointError(_e) => RpcResponse::error(id, "error parsing outpoint".to_string()),
+            PluginError::BitcoinAddressError(_e) => RpcResponse::error(id, "error parsing outpoint".to_string()),
+            PluginError::Http(_e) => RpcResponse::error(id, "http request error".to_string()), //TODO: give more http error info
+        }
     }
 }
