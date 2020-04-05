b4f00c1b9d17c2b6e142f0598c24030512a6e2cc
Domenico Gabriele <domenico@blockstream.com>
2020-04-05 04:13:57 +0200

    sign multiple inputs

--- src/sweep.rs
+++ src/sweep.rs
@@ -18,16 +18,16 @@ use std::str::FromStr;
 #[derive(Debug)]
 pub struct SweepData {
     private_key: PrivateKey,
-    utxo: TxOut,
-    outpoint: OutPoint,
+    utxo: Vec<TxOut>,
+    outpoint: Vec<OutPoint>,
 }
 
 impl SweepData {
     pub fn new(private_key: PrivateKey, utxo: TxOut, outpoint: OutPoint) -> SweepData {
         SweepData {
             private_key,
-            utxo,
-            outpoint,
+            utxo: vec![utxo],
+            outpoint: vec![outpoint],
         }
     }
 
@@ -50,76 +50,93 @@ impl SweepData {
         let mut j = Value::from_str(&r)?;
         // TODO: remove unwraps
         // use std::option::NoneError?
-        let obj: &Value = &j
+        let obj = j
             .as_array_mut()
-            .ok_or("None found while unwrapping option")?[0]; // Vec<Value>[0]
+            .ok_or("None found while unwrapping option")?; // Vec<Value>
 
-        let txid = obj
-            .get("txid")
-            .ok_or("Error getting key txid from object")?
-            .as_str()
-            .unwrap();
-        let vout = obj
-            .get("vout")
-            .ok_or("Error getting key vout from object")?
-            .as_u64()
-            .unwrap() as u32; // TODO: check for confirmation
-        let value = obj
-            .get("value")
-            .ok_or("Error getting key value from object")?
-            .as_u64()
-            .unwrap();
-        let outpoint = bitcoin::OutPoint::from_str(format!("{}:{}", txid, vout).as_str())?;
+        let mut utxos: Vec<TxOut> = Vec::with_capacity(obj.len());
+        let mut outpoints: Vec<OutPoint> = Vec::with_capacity(obj.len());
 
-        let utxo = TxOut {
-            script_pubkey: script,
-            value: value,
-        };
+        for o in obj {
+            let txid = o
+                .get("txid")
+                .ok_or("Error getting key txid from object")?
+                .as_str()
+                .unwrap();
+            let vout = o
+                .get("vout")
+                .ok_or("Error getting key vout from object")?
+                .as_u64()
+                .unwrap() as u32; // TODO: check for confirmation
+            let value = o
+                .get("value")
+                .ok_or("Error getting key value from object")?
+                .as_u64()
+                .unwrap();
+            let outpoint = bitcoin::OutPoint::from_str(format!("{}:{}", txid, vout).as_str())?;
+            outpoints.push(outpoint);
+
+            let utxo = TxOut {
+                script_pubkey: script.clone(),
+                value: value,
+            };
+            utxos.push(utxo);
+        }
 
         Ok(SweepData {
             private_key,
-            utxo,
-            outpoint,
+            utxo: utxos,
+            outpoint: outpoints,
         })
     }
 
     pub fn sweep(&self, dest: Address) -> Result<Transaction, PluginError> {
         let secp = Secp256k1::new();
         let dest_script = dest.script_pubkey();
-        let dest_value = self.utxo.value;
+        let mut dest_value: u64 = 0;
+
+        let mut tx = Transaction {
+            version: 2 as u32,
+            lock_time: 0 as u32,
+            input: vec![],
+            output: vec![],
+        };
+
+        for i in 0..self.utxo.len() {
+            let tx_in = TxIn {
+                previous_output: self.outpoint[i],
+                script_sig: Script::new(),
+                sequence: u32::max_value(),
+                witness: Vec::new(),
+            };
+            tx.input.push(tx_in);
+            dest_value += self.utxo[i].value
+        }
+
         let dest_out = TxOut {
             script_pubkey: dest_script.clone(),
             value: dest_value - 500 as u64, // TODO: allow to pass fee_rate
         };
 
-        let tx_in = TxIn {
-            previous_output: self.outpoint,
-            script_sig: Script::new(),
-            sequence: u32::max_value(),
-            witness: Vec::new(),
-        };
+        tx.output.push(dest_out);
 
-        let mut tx = Transaction {
-            version: 2 as u32,
-            lock_time: 0 as u32,
-            input: vec![tx_in.clone()],
-            output: vec![dest_out.clone()],
-        };
+        //sign inputs
+        for i in 0..tx.input.len() {
+            let hash = tx.signature_hash(i, &self.utxo[i].script_pubkey, SigHashType::All as u32);
+            let msg = &bitcoin::secp256k1::Message::from_slice(&hash.into_inner()[..])?; // to check
+            let k = &self.private_key.key;
+            let signature = secp.sign(msg, k);
+            let mut signature = signature.serialize_der().to_vec();
+            signature.push(SigHashType::All as u8);
 
-        let hash = tx.signature_hash(0, &self.utxo.script_pubkey, SigHashType::All as u32);
-        let msg = &bitcoin::secp256k1::Message::from_slice(&hash.into_inner()[..])?; // to check
-        let k = &self.private_key.key;
-        let signature = secp.sign(msg, k);
-        let mut signature = signature.serialize_der().to_vec();
-        signature.push(SigHashType::All as u8);
+            let pubkey = self.private_key.public_key(&secp);
+            let script_sig = script::Builder::new()
+                .push_slice(&signature)
+                .push_key(&pubkey)
+                .into_script();
 
-        let pubkey = self.private_key.public_key(&secp);
-        let script_sig = script::Builder::new()
-            .push_slice(&signature)
-            .push_key(&pubkey)
-            .into_script();
-
-        tx.input[0].script_sig = script_sig.clone();
+            tx.input[i].script_sig = script_sig.clone();
+        }
 
         Ok(tx)
     }
